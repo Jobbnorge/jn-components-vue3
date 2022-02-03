b91a6e59d6c30aa758b215dedd3167c2999c5ff0
Alexander Schmidt <Alexander@jobbnorge.no>
2022-02-03 10:54:48 +0100

    feat: Move validation logic from managecandidate to composables

---
+++ src/components/interview-slots/UseCreateSlotValidation.js
@@ -0,0 +1,60 @@
+import { computed } from "vue";
+
+function existsOnBatch(appId, batch) {
+  return batch.value.findIndex(({ appID }) => appID === appId) !== -1;
+}
+
+function useCreateSlotValidation(
+  selectedSlots,
+  displaySlot,
+  requiredNumberSlots,
+  numberExisistingSlots,
+  interviewBatch
+) {
+  return computed(() => {
+    let _selectedSlots = selectedSlots.value ? selectedSlots.value.length : 0;
+    let arr = [];
+    if (
+      displaySlot.value &&
+      requiredNumberSlots.value > numberExisistingSlots.value + _selectedSlots
+    )
+      arr.push(
+        `Du må velge minst ${
+          requiredNumberSlots.value -
+          (numberExisistingSlots.value + _selectedSlots)
+        } slot(s) til`
+      );
+    if (
+      interviewBatch.id === null &&
+      (interviewBatch.title == undefined || interviewBatch.title === "")
+    )
+      arr.push("Intervjurunden trenger et navn");
+    return arr;
+  });
+}
+
+// number selected candidates plus those allready existing on batch
+// without duplicates
+function useRequiredNumberSlots(selectedCandidates, applicationsOnBatch) {
+  if (!Object.prototype.isPrototypeOf.call(Array.prototype, selectedCandidates))
+    selectedCandidates = [{ appid: selectedCandidates.id }];
+
+  return computed(() => {
+    // remove appIds that exist in 'selectedCandidates' and those that does not allow slot selection
+    let arr = applicationsOnBatch.value.filter(
+      ({ appID, canChooseInterviewSlot }) =>
+        canChooseInterviewSlot &&
+        selectedCandidates.findIndex((candidate) => {
+          return candidate.appid === appID;
+        }) === -1
+    );
+    return (
+      arr.length +
+      selectedCandidates.filter(
+        ({ appid }) => !existsOnBatch(appid, applicationsOnBatch)
+      ).length
+    );
+  });
+}
+
+export { useCreateSlotValidation, useRequiredNumberSlots, existsOnBatch };
