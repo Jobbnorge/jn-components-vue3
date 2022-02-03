61acfa10c953f4aeecf629c647f334dcdbf94bc1
Alexander Schmidt <Alexander@jobbnorge.no>
2022-02-03 13:38:15 +0100

    feat: move ValidationError component

---
+++ src/components/interview-slots/ValidationError.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li
+      v-for="(error, index) in validationErrors"
+      :key="index"
+      style="color: red"
+    >
+      {{ error }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: { validationErrors: Array },
+  setup() {},
+};
+</script>
