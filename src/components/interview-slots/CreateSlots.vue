b39f01f309de87d595266168d9802dc67394d47e
Vilje Bech <viljebech@gmail.com>
2022-05-30 14:04:29 +0200

    fix:  trigger deleteAndRefetchSlots() every time slotsHasChanged.value changes

--- src/components/interview-slots/CreateSlots.vue
+++ src/components/interview-slots/CreateSlots.vue
@@ -185,10 +185,8 @@ export default {
 
     watch(
       () => slotsHasChanged.value,
-      (val) => {
-        if (val === true) {
-          deleteAndRefetchSlots();
-        }
+      () => {
+        deleteAndRefetchSlots();
       }
     );
     watch(
